60e2f1dc24e99803bbd82a910f467f95af8233fc
ChinZJ <122326502+ChinZJ@users.noreply.github.com>
2025-06-02 23:45:49 +0800

    Add getComputerChoice logic

--- rock-paper-scissors/logic.js
+++ rock-paper-scissors/logic.js
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+const choiceConverter = ["rock", "paper", "scissors"];
+const choiceLength = choiceConverter.length;
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+// Alternatively, use if else statements instead of arrays.
+function getComputerChoice() {
+    return choiceConverter[getRandomInt(choiceLength)];
+}
+
+console.log(getComputerChoice());
