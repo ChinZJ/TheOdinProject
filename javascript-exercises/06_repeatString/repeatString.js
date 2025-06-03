e07ab511caa217ccc50156bde2f15711932902c6
ChinZJ <122326502+ChinZJ@users.noreply.github.com>
2025-06-04 00:28:31 +0800

    Complete 06_repeatString

--- javascript-exercises/06_repeatString/repeatString.js
+++ javascript-exercises/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, numTimes) {
+    if (numTimes < 0) {
+        return "ERROR";
+    }
+    let answer = "";
+    for (let i = 0; i < numTimes; i++) {
+        answer += string;
+    }
+    return answer;
 };
 
 // Do not edit below this line
