7085b481cb91b7662713dc3866b03ac5ddbd5ec8
ChinZJ <122326502+ChinZJ@users.noreply.github.com>
2025-06-04 00:51:28 +0800

    Complete 09_sumAll

--- javascript-exercises/09_sumAll/sumAll.js
+++ javascript-exercises/09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if ((numOne < 0) || (!Number.isInteger(numOne)) ||
+            (numTwo < 0) || (!Number.isInteger(numTwo))) {
+        return "ERROR";
+    }
 
+    let start, end;
+    if (numOne > numTwo) {
+        start = numTwo;
+        end = numOne;
+    } else {
+        start = numOne;
+        end = numTwo;
+    }
+
+    let accumulator = 0;
+    for (; start <= end; start++) {
+        accumulator += start;
+    }
+    return accumulator;
 };
 
 // Do not edit below this line
