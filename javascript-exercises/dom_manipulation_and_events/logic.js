6883645982dc43f0e2a5536da237830e049280f2
ChinZJ <122326502+ChinZJ@users.noreply.github.com>
2025-06-04 15:15:04 +0800

    Create template for dom_manipulation_and_events exercise

---
+++ javascript-exercises/dom_manipulation_and_events/logic.js
@@ -0,0 +1,30 @@
+// Reference to container div that exists within HTML
+const container = document.querySelector("#container");
+
+// Create a new div called content
+const content = document.createElement("div");
+// Add a class called content.
+content.classList.add("content");
+// Add text to the content.
+content.textContent = "This is the glorious text-content!";
+
+// Append the div to container
+container.appendChild(content);
+
+// Add the following to the container using ONLY JavaScript and the DOM methods
+
+// a <p> with red text that says “Hey I’m red!”
+
+
+// An <h3> with blue text that says “I’m a blue h3!”
+
+
+// A <div> with a black border and pink background color with the following elements inside of it:
+// (Hint: After creating the <div> with with createElement, append the <h1> and <p> to it before adding it to the container.)
+
+
+// Another <h1> that says “I’m in a div”
+
+
+// A <p> that says “ME TOO!”
+
