111125769b979c51c108c4f49bca2c6c29d5a30a
ChinZJ <122326502+ChinZJ@users.noreply.github.com>
2025-06-06 15:24:42 +0800

    Complete 16_findTheOldest

--- javascript-exercises/16_findTheOldest/findTheOldest.js
+++ javascript-exercises/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const date = new Date();
 
+const findTheOldest = function(personList) {
+    let currOldest = 0;
+    let oldestPerson;
+
+    for (let person of personList) {
+        let birthYear = person.yearOfBirth;
+        let deathYear = person.yearOfDeath;
+        if (deathYear === undefined) {
+            deathYear = date.getFullYear();
+        }
+        let lifespan = deathYear - birthYear;
+
+        if (lifespan >= currOldest) {
+            currOldest = lifespan;
+            oldestPerson = person;
+        }
+    }
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
